ec1d07d4b03302095692f75b6353b5ca0152a96a
Vaibhav Nangia <vaibhavnangia@TRKINC-MAC001.local>
2025-07-05 00:40:34 +0530

    fix: BE add conversation to db

--- backend/msgs.controllers.js
+++ backend/msgs.controllers.js
@@ -14,3 +14,17 @@ export const addMsgConversation = async (participants, msg) => {
         console.log(`error occured while sending message`, err)
     }
 }
+
+export const getMsgsForConversation = async (req, res) => {
+    try {
+        const { sender, receiver } = req.query;
+        const participants = [sender, receiver];
+        const conversation = await Conversation.findOne({users: { $all: participants}});
+        if(!conversation) {
+            console.log("no conversation found");
+        }
+        return res.json(conversation.msgs);
+    } catch(err) {
+        console.log("error occured while fetching messages", err)
+    }
+}
